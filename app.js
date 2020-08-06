3693ae19a4d055f7ac371b250a43700094d974d5
MeyerFrenchel <62379903+MeyerFrenchel@users.noreply.github.com>
2020-08-06 22:20:12 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+$(function () {
+    $('[data-toggle="popover"]').popover()
+  })
+
+  function normalizeSlideHeights() {
+    $('.carousel').each(function(){
+      var items = $('.carousel-item', this);
+      // reset the height
+      items.css('min-height', 0);
+      // set the height
+      var maxHeight = Math.max.apply(null, 
+          items.map(function(){
+              return $(this).outerHeight()}).get() );
+      items.css('min-height', maxHeight + 'px');
+    })
+}
+
+$(window).on(
+    'load resize orientationchange', 
+    normalizeSlideHeights);
